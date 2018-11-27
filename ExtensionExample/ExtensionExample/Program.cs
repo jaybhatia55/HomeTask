38c46766c296d62abde6e152918b79733cabac38
JAY BHATIA <jaybhatia55@yahoo.com>
2018-11-28 00:29:41 +0530

    Extension method added

---
+++ ExtensionExample/ExtensionExample/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionExample
+{
+    class Math
+    {
+        public void Sum()
+        {
+            Console.WriteLine("\nSUM method");
+        }
+        public void Sub()
+        {
+            Console.Write("\nSUB Method");
+        }
+    }
+    static class MathExtension
+    {
+        public static void Div(this Math mathobj1 )
+        {
+            Console.WriteLine("\nDIV method");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math mathobj = new Math();
+            mathobj.Sub();
+            mathobj.Sum();
+            mathobj.Div();
+            Console.ReadKey();
+        }
+    }
+}
